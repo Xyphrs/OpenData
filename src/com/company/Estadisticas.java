66d36f265230192a69d0ff3a07e8c652f1ae29ce
Xyphrs <dromerof01@elpuig.xeill.net>
2021-02-02 20:13:54 +0100

    URL csv Commit

--- src/com/company/Estadisticas.java
+++ src/com/company/Estadisticas.java
@@ -1,112 +1,74 @@
 package com.company;
 
 public class Estadisticas implements Comparable<Estadisticas> {
-    private String hometeam;
-    private String visitorteam;
-    private String matchdate;
-    private int goals_home;
-    private int visitor_home;
-    private int wins_home;
-    private int loses_home;
-    private int wins_visitant;
-    private int loses_visitant;
+    private int objectid;
+    private String zona;
+    private float AROPE; // At risk of Poverty
+    private float area;
+    private float length;
 
-    public Estadisticas(String hometeam, String visitorteam, String matchdate, int goals_home, int visitor_home, int wins_home, int loses_home, int wins_visitant, int loses_visitant) {
-        this.hometeam = hometeam;
-        this.visitorteam = visitorteam;
-        this.matchdate = matchdate;
-        this.goals_home = goals_home;
-        this.visitor_home = visitor_home;
-        this.wins_home = wins_home;
-        this.loses_home = loses_home;
-        this.wins_visitant = wins_visitant;
-        this.loses_visitant = loses_visitant;
+    public Estadisticas(int objectid, String zona, float AROPE, float area, float length) {
+        this.objectid = objectid;
+        this.zona = zona;
+        this.AROPE = AROPE;
+        this.area = area;
+        this.length = length;
     }
 
-    public String getHometeam() {
-        return hometeam;
+    public int getObjectid() {
+        return objectid;
     }
 
-    public void setHometeam(String hometeam) {
-        this.hometeam = hometeam;
+    public void setObjectid(int objectid) {
+        this.objectid = objectid;
     }
 
-    public String getVisitorteam() {
-        return visitorteam;
+    public String getZona() {
+        return zona;
     }
 
-    public void setVisitorteam(String visitorteam) {
-        this.visitorteam = visitorteam;
+    public void setZona(String zona) {
+        this.zona = zona;
     }
 
-    public String getMatchdate() {
-        return matchdate;
+    public float getAROPE() {
+        return AROPE;
     }
 
-    public void setMatchdate(String matchdate) {
-        this.matchdate = matchdate;
+    public void setAROPE(float AROPE) {
+        this.AROPE = AROPE;
     }
 
-    public int getGoals_home() {
-        return goals_home;
+    public float getArea() {
+        return area;
     }
 
-    public void setGoals_home(int goals_home) {
-        this.goals_home = goals_home;
+    public void setArea(float area) {
+        this.area = area;
     }
 
-    public int getVisitor_home() {
-        return visitor_home;
+    public float getLength() {
+        return length;
     }
 
-    public void setVisitor_home(int visitor_home) {
-        this.visitor_home = visitor_home;
-    }
-
-    public int getWins_home() {
-        return wins_home;
-    }
-
-    public void setWins_home(int wins_home) {
-        this.wins_home = wins_home;
-    }
-
-    public int getLoses_home() {
-        return loses_home;
-    }
-
-    public void setLoses_home(int loses_home) {
-        this.loses_home = loses_home;
-    }
-
-    public int getWins_visitant() {
-        return wins_visitant;
-    }
-
-    public void setWins_visitant(int wins_visitant) {
-        this.wins_visitant = wins_visitant;
-    }
-
-    public int getLoses_visitant() {
-        return loses_visitant;
-    }
-
-    public void setLoses_visitant(int loses_visitant) {
-        this.loses_visitant = loses_visitant;
+    public void setLength(float length) {
+        this.length = length;
     }
 
     @Override
     public String toString() {
-        return "\nEquipo Local = '" + hometeam + '\'' + " \nEquipo Visitante = '" + visitorteam + '\'' + " \nFecha del Partido = '" +
-                matchdate + '\'' + " \nGoles Local = " + goals_home + " \nGoles Visitante = " + visitor_home + " \nVictorias Equipo Local = " +
-                wins_home + " \nDerrotas Equipo Local = " + loses_home + " \nVictorias Equipo Visitante = " +
-                wins_visitant + " \nDerrotas Equipo Visitante = " + loses_visitant +"\n ----------------";
+        return "ID: " + objectid +
+                "\nZona: " + zona +
+                "\nAROPE: " + AROPE +
+                "\nArea: " + area +
+                "\nLength: " + length +
+                "\n------------------";
     }
 
     @Override
     public int compareTo(Estadisticas o) {
-        if(getLoses_home() < o.getLoses_home()) return -1;
-        else if(getLoses_home() > o.getLoses_home()) return 1;
+        if(getObjectid() > o.getObjectid()) return -1;
+        else if(getObjectid() < o.getObjectid()) return 1;
         else return 0;
     }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,14 @@
 package com.company;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    static final String path = "https://opendata.arcgis.com/datasets/ace07e97d10a4ab28b336dd67e42c5c6_0.csv";
+
 
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
@@ -20,27 +20,27 @@ public class Main {
         do {
             System.out.println("");
             System.out.println("Selecciona una opcion del menu para consultar datos");
-            System.out.println("1. Partidos en el que el equipo Local ha marcado mas de 1 (Filter)");
-            System.out.println("2. Mostrar el nombre de los Equipos Locales (Map)");
-            System.out.println("3. Equipo local con el menor de derrotas (Min)");
-            System.out.println("4. Equipo local con el maximo de derrotas (Max)");
-            System.out.println("5. Ordenar por las partidas perdidas de los equipos (Sort)");
-            System.out.println("6. Cuantos partidos han jugado los Equipos locales (Count)");
-            System.out.println("7. Mostrar el nombre de los Equipos Locales sin repeticiones (Distinct)");
+            System.out.println("1. Mostrar la informacion de todos los campos en los cuales la id no sea menor a 7(Filter)");
+            System.out.println("2. Mostrar el nombre de las zonas (Map)");
+            System.out.println("3. ID mas baja (Min)");
+            System.out.println("4. ID mas alta (Max)");
+            System.out.println("5. Ordenar por id, de mayor a menor (Sort)");
+            System.out.println("6. Cuantas zonas hay en el fichero csv (Count)");
+            System.out.println("7. Mostrar el nombre de las zonas sin repeticiones (Distinct)");
             System.out.println("8. Ver CSV");
             System.out.println("9. Salir");
             seleccion = scanner.nextInt();
             switch (seleccion) {
-                case 1 -> statList.stream().filter(p -> p.getGoals_home() > 1).forEach(System.out::println);
-                case 2 -> statList.stream().map(Estadisticas::getHometeam).forEach(System.out::println);
+                case 1 -> statList.stream().filter(p -> p.getObjectid() > 7f).forEach(System.out::println);
+                case 2 -> statList.stream().map(Estadisticas::getZona).forEach(System.out::println);
                 case 3 -> System.out.println(statList.stream().min(Estadisticas::compareTo));
                 case 4 -> System.out.println(statList.stream().max(Estadisticas::compareTo));
                 case 5 -> {
                     statList.sort(Estadisticas::compareTo);
                     System.out.println(statList);
                 }
-                case 6 -> System.out.println(statList.stream().map(Estadisticas::getHometeam).count());
-                case 7 -> statList.stream().map(Estadisticas::getHometeam).distinct().forEach(System.out::println);
+                case 6 -> System.out.println(statList.stream().map(Estadisticas::getZona).count());
+                case 7 -> statList.stream().map(Estadisticas::getZona).distinct().forEach(System.out::println);
                 case 8 -> statList.forEach(System.out::println);
                 case 9 -> System.out.println("Adios...");
                 default -> System.out.println("Introduce un numero entre 1-8");
@@ -49,8 +49,9 @@ public class Main {
     }
 
     private static void readCSV(List<Estadisticas> statList) throws IOException {
-        File file = new File("estadistiques.csv");
-        BufferedReader br = new BufferedReader(new FileReader(file));
+        URL url = new URL(path);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
         br.readLine();
         String line;
         line = br.readLine();
@@ -58,7 +59,7 @@ public class Main {
             while (line != null) {
                 String[] data = line.split(",");
                 for (int i = 0; i < 1; i++) {
-                    statList.add(new Estadisticas(data[0], data[1], data[2], Integer.parseInt(data[3]), Integer.parseInt(data[4]), Integer.parseInt(data[5]), Integer.parseInt(data[6]), Integer.parseInt(data[7]), Integer.parseInt(data[8])));
+                    statList.add(new Estadisticas(Integer.parseInt(data[0]), data[1], Float.parseFloat(data[2]), Float.parseFloat(data[3]), Float.parseFloat(data[4])));
                 }
                 line = br.readLine();
                 System.out.println("------------------");
